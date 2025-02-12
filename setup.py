a8cf4e15dc4f13808d78868279f04f60636dbf74
PowerfulBean <25721564+powerfulbean@users.noreply.github.com>
2025-02-12 03:36:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import setuptools
 
 
 setuptools.setup(
-  name="DynamicallyWarpedTRF",
+  name="dynamically_warped_trf",
   version="0.0.1",
   author="Powerfulbean",
   author_email="powerfulbean@gmail.com",
